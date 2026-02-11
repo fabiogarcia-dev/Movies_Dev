8e0caa6bc4dbd8f4b4a7b1712bfd73df24cb6d62
fabiogarcia-dev <fabio.ti.am@gmail.com>
2026-02-11 18:52:19 -0400

    componente Header estruturado e estilizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,37 @@
 import Logo from "../../assets/logo.png"
+import { Container, Li, Menu } from "./styles";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+    const [changeBackground, setChangeBackground] = useState(false);
+    const { pathname } = useLocation();
+
+    window.onscroll = () =>{
+        if(!changeBackground && window.pageYOffset > 150){
+            setChangeBackground(true);
+        }
+        if(changeBackground && window.pageYOffset <= 150){
+            setChangeBackground(false);
+        }
+    }
+    
     return(
-        <div>
-            <img src={Logo} alt="logo-devMovies" style={{height:"50px", width:"250px"}}/>
-        </div>
+        <Container changeBackground={changeBackground}>
+            <img src={Logo} alt="Logo" />
+            <Menu>
+                <Li isActive={pathname === "/"}> 
+                    <Link to="/">Home</Link>
+                </Li>
+                <Li isActive={pathname.includes("filmes")}>
+                    <Link to="/filmes">Filmes</Link>
+                </Li>
+                <Li isActive={pathname.includes("series")}>
+                    <Link to="/series">Séries</Link>
+                </Li>
+            </Menu>
+            
+        </Container>
     )
 }
 export default Header
